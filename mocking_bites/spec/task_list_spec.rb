1a2ef86d0e290ab6dc2c05c597b2eafe6d658bcd
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-22 15:01:15 +0100

    unit testing parent child

--- mocking_bites/spec/task_list_spec.rb
+++ mocking_bites/spec/task_list_spec.rb
@@ -1,5 +1,6 @@
 require "task_list"
 
+
 RSpec.describe TaskList do
   it "initially has an empty list" do
     task_list = TaskList.new
@@ -12,4 +13,25 @@ RSpec.describe TaskList do
   end
 
   # Unit test `#all` and `#all_complete?` behaviour
+  it "adds tasks to a list" do
+    task_list = TaskList.new
+    task_1 = double(:asd)
+    task_2 = double(:asd)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    expect(task_list.all).to eq [task_1, task_2]
+  end
+
+  it "marks tasks as complete" do
+    task_list = TaskList.new
+    task_1 = double(:fake_task, mark_complete: nil, complete?: true)
+    task_2 = double(:fake_task, mark_complete: nil, complete?: true)
+    task_list.add(task_1)
+    task_list.add(task_2)
+    task_1.mark_complete
+    task_2.mark_complete
+    expect(task_list.all_complete?).to eq true
+  end
+
+
 end
